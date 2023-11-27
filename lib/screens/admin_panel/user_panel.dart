d3d1d3741cfa7d89c7bed8440c7496c2a37d8a36
Wojciech Buńka <wojtek.bunka@gmail.com>
2023-11-27 20:03:56 +0100

    wip

--- lib/screens/admin_panel/user_panel.dart
+++ lib/screens/admin_panel/user_panel.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:hasior_flutter/classes/globalSnackbar.dart';
 import 'package:hasior_flutter/classes/language_constants.dart';
 import 'package:hasior_flutter/extensions/string_capitalize.dart';
 import 'package:hasior_flutter/models/calendar.dart';
 import 'package:hasior_flutter/models/calendarList.dart';
 import 'package:hasior_flutter/models/userWithToken.dart';
-import 'package:hasior_flutter/services/api_service.dart';
 import 'package:hasior_flutter/theme.dart';
-import 'package:hasior_flutter/widgets/calendar_widget.dart';
 import 'package:hasior_flutter/widgets/navigator_drawer.dart';
 
 class UserPanel extends StatefulWidget {
@@ -49,74 +46,78 @@ class _UserPanelState extends State<UserPanel> {
         ),
       ),
       extendBody: true,
-      body: tableComponent(),
-      bottomNavigationBar: bottomNavigationBar(),
+      body: bodyContainer(),
     );
   }
 
-  Table tableComponent() {
-    return Table(
-      border: TableBorder.all(),
-      //columnWidths: const <int, TableColumnWidth>{
-      //0: IntrinsicColumnWidth(),
-      //1: FlexColumnWidth(),
-      //2: FixedColumnWidth(64),
-      //},
-      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-      children: <TableRow>[
-        createTableRow(["Hej1", "Hej2", "Hej3", "Hej4"])
+  SizedBox bodyContainer() {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: tableComponent(),
+    );
+  }
+
+  DataTable tableComponent() {
+    return DataTable(
+      columns: generateTableColumns(),
+      rows: [
+        testRow(),
+        testRow(),
       ],
     );
   }
 
-  TableRow createTableRow(List<String> names) {
-    return TableRow(
-      children: names.map((cell) => createCell(cell)).toList()
+  DataRow testRow() {
+    return DataRow(
+      cells: [
+        DataCell(Text('Przykładowa Osoba')),
+        DataCell(Text('przykladowy@email.com')),
+        DataCell(Wrap(
+          spacing: 5,
+          children: [
+            Chip(label: Text('Administrator')),
+            Chip(label: Text('Użytkownik')),
+          ],
+        )),
+        actionButtons()
+      ],
     );
   }
 
-  TableCell createCell(String cellName) {
-    return TableCell(
-      verticalAlignment: TableCellVerticalAlignment.top,
-      child: Text(cellName),
-    );
+  List<DataColumn> generateTableColumns() {
+    return [
+      const DataColumn(label: Text('Nazwa')),
+      const DataColumn(label: Text('Email')),
+      const DataColumn(label: Text('Role')),
+      const DataColumn(label: Text('Akcje'))
+    ];
   }
 
-  Widget? bottomNavigationBar() {
-    return user != null
-        ? Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.transparent, primaryBlack.withOpacity(0.4)],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                stops: const [0.0, 1.0],
-                tileMode: TileMode.clamp,
-              ),
-            ),
-            child: Container(
-                margin: const EdgeInsets.only(bottom: 10, left: 10, right: 10),
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  color: primaryBlack,
-                ),
-                child: BottomNavigationBar(
-                  backgroundColor: Colors.transparent,
-                  currentIndex: currentIndex,
-                  onTap: (index) => setState(() {
-                    currentIndex = index;
-                  }),
-                  items: [
-                    BottomNavigationBarItem(
-                        icon: const Icon(Icons.calendar_month),
-                        label: translation(context).all_events.capitalize()),
-                    BottomNavigationBarItem(
-                        icon: const Icon(Icons.favorite),
-                        label:
-                            translation(context).favorite_events.capitalize()),
-                  ],
-                )),
-          )
-        : null;
+  DataCell actionButtons() {
+    const editUser = 'Edytuj użytkownika';
+    const deleteUser = 'Akcja 1';
+
+    return DataCell(Align(
+      alignment: Alignment.centerRight,
+      child: Row(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              // Obsługa przycisku 1
+            },
+            child: const Text(editUser),
+          ),
+          const SizedBox(width: 8),
+          ElevatedButton(
+            onPressed: () {
+              // Obsługa przycisku 2
+            },
+            child: const Text(deleteUser),
+          ),
+        ],
+      ),
+    ));
   }
+
 }
